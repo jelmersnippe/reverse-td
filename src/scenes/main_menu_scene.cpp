eab982fc3da4bba075aac7c9105ca15160d8739d
Jelmer Snippe <jelmersnippe@hotmail.com>
2026-06-07 19:57:05 +0200

    Use new layouting for main menu

--- src/scenes/main_menu_scene.cpp
+++ src/scenes/main_menu_scene.cpp
@@ -9,35 +9,39 @@
 #include "systems/scene_manager.hpp"
 
 namespace {
-const UI::ElementStyle BUTTON_STYLE = {.font_size = 24,
-                                       .padding = 8,
+const UI::ElementStyle BUTTON_STYLE = {.justify_content = UI::JustifyContent::CENTER,
+                                       .padding = 20,
                                        .width = 400,
-                                       .height = 100,
                                        .color = {.border = BLACK, .background = WHITE, .text = BLACK},
                                        .color_hover = {.border = BLACK, .background = GRAY, .text = WHITE},
                                        .color_active = {.border = BLACK, .background = DARKGRAY, .text = WHITE}};
+const UI::ElementStyle BUTTONTEXT_STYLE = {
+    .font_size = 24,
+};
 UI ui = {};
 
 void Draw(GameState& state) {
     ClearBackground(GRAY);
 
     ui.begin_ui(Vec2{});
-    ui.begin_layout("layout_main_menu",
-                    {.direction = UI::LayoutDirection::Vertical, .width = SCREEN_WIDTH, .height = SCREEN_HEIGHT});
+    ui.begin_layout("layout_main_menu", {.direction = UI::LayoutDirection::Vertical,
+                                         .justify_content = UI::JustifyContent::CENTER,
+                                         .width = SCREEN_WIDTH,
+                                         .height = SCREEN_HEIGHT});
 
     ui.text("txt_title", "REVERSE TIDDY", {.font_size = 40});
 
     if (ui.begin_button("btn_start", BUTTON_STYLE)) SCENE_MANAGER.SetScene(state, GAME_SCENE);
-    ui.text("txt_start", "Start", {});
+    ui.text("txt_start", "Start", BUTTONTEXT_STYLE);
     ui.end_button();
     if (ui.begin_button("btn_test", BUTTON_STYLE)) SCENE_MANAGER.SetScene(state, TEST_SCENE);
-    ui.text("txt_test", "Test", {});
+    ui.text("txt_test", "Test", BUTTONTEXT_STYLE);
     ui.end_button();
     if (ui.begin_button("btn_ui", BUTTON_STYLE)) SCENE_MANAGER.SetScene(state, UI_SCENE);
-    ui.text("txt_ui", "Ui", {});
+    ui.text("txt_ui", "Ui", BUTTONTEXT_STYLE);
     ui.end_button();
     if (ui.begin_button("btn_quit", BUTTON_STYLE)) state.should_exit = true;
-    ui.text("txt_quit", "Quit", {});
+    ui.text("txt_quit", "Quit", BUTTONTEXT_STYLE);
     ui.end_button();
 
     ui.end_layout();
