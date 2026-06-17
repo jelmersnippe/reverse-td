8223a8b83face26f63ea4e8b531dad76f524119a
Jelmer Snippe <jelmersnippe@hotmail.com>
2026-06-17 16:47:56 +0200

    Fix random float for negative values

--- src/core/random.hpp
+++ src/core/random.hpp
@@ -9,5 +9,5 @@ inline int random_int(int min, int max) {
 inline float random_float(float min, float max) {
     const float random = (float)GetRandomValue(0, 100) * 0.01f;
 
-    return min + max * random;
+    return min + (max - min) * random;
 }
