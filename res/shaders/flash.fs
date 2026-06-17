0646817fec5eccc69f9fd038b06c943389022d73
Jelmer Snippe <jelmersnippe@hotmail.com>
2026-06-17 16:47:03 +0200

    Change shader to opengl 1 for web

--- res/shaders/flash.fs
+++ res/shaders/flash.fs
@@ -1,17 +1,17 @@
-#version 330
+#version 100
 
-in vec2 fragTexCoord;
+precision mediump float;
+
+varying vec2 fragTexCoord;
 
 uniform sampler2D texture0;
 uniform vec3 color;
-uniform float volume; // 0 = normal, 1 = fully white
-
-out vec4 finalColor;
+uniform float volume;
 
 void main()
 {
-    vec4 texel = texture(texture0, fragTexCoord);
+    vec4 texel = texture2D(texture0, fragTexCoord);
 
-    finalColor.rgb = mix(texel.rgb, color, volume);
-    finalColor.a = texel.a;
+    vec3 finalRgb = mix(texel.rgb, color, volume);
+    gl_FragColor = vec4(finalRgb, texel.a);
 }
